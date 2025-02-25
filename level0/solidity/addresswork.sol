e14ecfac0d675cb05a0e085a5aff99069425c5f3
muyangyuchen <90684553+muyangyuchen@users.noreply.github.com>
2025-02-25 20:59:14 +0800

    Update addresswork.sol

--- level0/solidity/addresswork.sol
+++ level0/solidity/addresswork.sol
@@ -1,1 +1,39 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SimpleWallet {
+    mapping (address => uint256) public balances;
+    mapping (address => bool) public whitelist;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function addWhitelist(address _wallet) external onlyOwner {
+        whitelist[_wallet] = true;
+    }
+
+    function getMymoney() external view returns (uint256) {
+        return balances[msg.sender];
+    }
+
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance.");
+        require(whitelist[msg.sender], "black");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function deposite() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+}
