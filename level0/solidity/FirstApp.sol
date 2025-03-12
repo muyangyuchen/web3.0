ed56815f14add187fc1a9cdbfa161cf1917eb5f8
muyangyuchen <90684553+muyangyuchen@users.noreply.github.com>
2025-03-12 17:18:25 +0800

    Update FirstApp.sol

--- level0/solidity/FirstApp.sol
+++ level0/solidity/FirstApp.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Counter {
+    uint256 private _counter;
+
+    constructor(uint256 counter) {
+        _counter = counter;
+    }
+
+    function get() public view returns (uint counter) {
+        return _counter;
+    }
+
+    function inc() public {
+        _counter++;
+    }
+
+    function dec() public {
+        _counter--;
+    }
+}
