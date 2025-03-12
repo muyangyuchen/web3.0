ceda611ce2b9586a6d9d4928a162949a8d45ad3f
muyangyuchen <90684553+muyangyuchen@users.noreply.github.com>
2025-03-12 17:50:18 +0800

    Update Constants.sol

--- level0/solidity/Constants.sol
+++ level0/solidity/Constants.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+contract Constants {
+    uint256 public constant MY_CONSTANT = 993456;  // in Wei
+    uint256 public constant TWO = 2;
+    address public constant zeroAddress = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+}
+
+contract Imuttable {
+    address public immutable myAddress = msg.sender;
+    uint256 public immutable MY_UINT;
+
+    constructor(uint256 _myUint) {
+        myAddress = msg.sender;
+        MY_UINT = _myUint;
+    }
+}
